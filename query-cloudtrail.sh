7677c67293ccd695a96ad6c9f867c5f30e918ffa
M. Adam Kendall <michael.kendall@gsa.gov>
2018-03-06 14:24:56 -0500

    Update pipeline to fix some formatting issues

--- query-cloudtrail.sh
+++ query-cloudtrail.sh
@@ -3,3 +3,6 @@ set -eux
 date
 aws cloudtrail lookup-events --region us-gov-west-1 --lookup-attributes AttributeKey=ResourceName,AttributeValue=${BUCKET} \
   | jq '.Events[] | [{id: .EventId, type: .EventName, timestamp: .EventTime, user: .Username}]'
+
+{ set +x; } 2> /dev/null # silently disable xtrace
+sleep 5
