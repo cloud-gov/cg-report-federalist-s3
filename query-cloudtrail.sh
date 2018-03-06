f0cc51278f400f9154bd7d8d91e32d0605883b37
M. Adam Kendall <michael.kendall@gsa.gov>
2018-03-06 14:48:24 -0500

    Cleanup the query script again

--- query-cloudtrail.sh
+++ query-cloudtrail.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -eu
-
-echo "Querying CloudTrail @ $(date) for bucket ${BUCKET}"
 echo
+echo "Date:         $(date)"
+echo "S3 bucket:    ${BUCKET}"
+echo
+set -x
 aws cloudtrail lookup-events --region us-gov-west-1 --lookup-attributes AttributeKey=ResourceName,AttributeValue=${BUCKET} \
   | jq '.Events[] | [{EventId: .EventId, EventName: .EventName, EventTime: .EventTime, Username: .Username}]'
 
+{ set +x; } 2> /dev/null # silently disable xtrace
+echo
 sleep 5
