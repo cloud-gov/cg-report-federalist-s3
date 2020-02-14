653e8dc4ae75325dc7ee7758721c9bc5852fd692
Tammer Saleh <tammer@superorbit.al>
2020-02-14 17:45:57 +0000

    Add test against json before emailing

---
+++ validate-json-spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'ostruct'
+require 'json'
+
+# To run tests:
+#   bundle install
+#   export JSON_FILE=...
+#   bundle exec rspec ./validate-json-spec.rb
+
+# JSON_FILE is set via the pipeline task definition.
+describe "#{ENV.fetch('JSON_FILE')}" do
+  let(:json) { File.read(ENV['JSON_FILE']) }
+  let(:events) { JSON.parse(json, object_class: OpenStruct) }
+
+  let(:federalist_bucket_events) do
+    events.select { |e| e.bucket_name.start_with?('federalist-') }
+  end
+
+  let(:cg_bucket_events) do
+    events.select { |e| e.bucket_name.start_with?('cg-') }
+  end
+
+  it 'contains events for federalist buckets' do
+    expect(federalist_bucket_events).not_to be_empty
+  end
+
+  it 'contains events for cg buckets' do
+    expect(cg_bucket_events).not_to be_empty
+  end
+end
